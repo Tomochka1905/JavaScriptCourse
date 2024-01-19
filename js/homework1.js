c9ad07c360d4b8fc7e5a2de52d025dbcfa5ef6e8
Olena Tomkevych <olenatomkevych1905@gmail.com>
2024-01-18 20:04:32 -0600

    Olena Tomkevych lesson 1

---
+++ js/homework1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let a = 23;
+let b = 29;
+let Sum = a + b;
+console.log(Sum);
+
+
+let d = 20;
+let c = 3;
+let Divide = d / c;
+console.log(Divide);
+
+let k = 3;
+let m = 3;
+let Multiply = k * m;
+console.log(Multiply);
+
+let s = 6;
+let exponetiation = s ** 6;
+console.log(exponentiation)
