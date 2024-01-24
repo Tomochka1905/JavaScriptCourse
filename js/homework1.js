7b2094131e48414cd20a2d2f8bbc894a2f1fffbb
Olena Tomkevych <olenatomkevych1905@gmail.com>
2024-01-23 20:47:41 -0600

    re-done

--- js/homework1.js
+++ js/homework1.js
@@ -1,21 +1,15 @@
 "use strict";
 
-let a = 23;
-let b = 29;
-let Sum = a + b;
-console.log(Sum);
+let a = 5;
+let b = 2;
+let sum = a + b;
+console.log(sum);
 
+let division = a / b;
+console.log(division);
 
-let d = 20;
-let c = 3;
-let Divide = d / c;
-console.log(Divide);
+let multiplication = a * b;
+console.log(multiplication);
 
-let k = 3;
-let m = 3;
-let Multiply = k * m;
-console.log(Multiply);
-
-let s = 6;
-let exponetiation = s ** 6;
+let exponentiation = a ** 2;
 console.log(exponentiation);
